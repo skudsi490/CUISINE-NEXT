f3b0acb6ab036bfcd686ebbd149d922a1a5a0f21
Sami Kudsi <skudsi490@gmail.com>
2024-03-17 23:51:49 +0100

    Finishing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,11 +5,8 @@ function Hero() {
   return (
     <header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
       <div className="flex-1 flex flex-col gap-10">
-        {/* Use Link without <a> tag */}
         <Link href="/">
-    
             <Image src="/logo.svg" alt="logo" width={201} height={196} priority />
-  
         </Link>
         <h1 className="text-4xl lg:text-5xl font-bold text-white leading-tight text-center shadow-lg">
           Explore The{" "}
@@ -20,8 +17,8 @@ function Hero() {
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh]">
-        <Image src="/anime.png" alt="anime" layout='fill' objectFit='cover' priority />
-      </div>
+      <Image src="/anime.png" alt="anime" width={1920} height={1080} />
+            </div>
     </header>
   );
 }
