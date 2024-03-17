11e746bb4d900419c8e02415511af5a70b7d23ad
Sami Kudsi <skudsi490@gmail.com>
2024-03-17 23:32:37 +0100

    Adding Social Links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
       <p className="text-base font-bold text-white">@2024 Sami Kudsi</p>
-      <Image src="/logo.svg" alt="EpicAnimeVault logo" width={47} height={44} />
+      <Image src="/logo.svg" alt="Cuisine logo" width={47} height={44} />
       <div className="flex items-center gap-6">
-        <Image src="/tiktok.svg" alt="TikTok logo" width={19} height={19} />
-        <Image src="/instagram.svg" alt="Instagram logo" width={19} height={19} />
-        <Image src="/twitter.svg" alt="Twitter logo" width={19} height={19} />
+        <a href="https://www.linkedin.com/in/sami-kudsi-0b1010164/" target="_blank" rel="noopener noreferrer">
+          <Image src="/bi_linkedin.svg" alt="LinkedIn logo" width={24} height={24} />
+        </a>
+        <a href="https://github.com/skudsi490" target="_blank" rel="noopener noreferrer">
+          <Image src="/fa_github.svg" alt="GitHub logo" width={24} height={24} />
+        </a>
       </div>
     </footer>
   );
